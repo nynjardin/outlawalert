7d1e55cdc76dd8db5e023611240a6a137bb9f4de
nynjardin <nynjardin@gmail.com>
2017-03-28 18:06:24 +1100

    Add files via upload

--- outlawalert/client.lua
+++ outlawalert/client.lua
@@ -6,6 +6,7 @@ AddEventHandler('thiefPlace', function(tx, ty, tz)
     SetBlipSprite(thiefBlip,  10)
     SetBlipColour(thiefBlip,  1)
     SetBlipAlpha(thiefBlip,  transT)
+    SetBlipAsShortRange(thiefBlip,  1)
     while transT ~= 0 do
         Wait(40)
         transT = transT - 1
@@ -15,18 +16,6 @@ AddEventHandler('thiefPlace', function(tx, ty, tz)
         return end
 end)
 
---[[Citizen.CreateThread( function()
-    while true do
-        Wait(0)
-        if transT ~= 0 then
-            transT = transT - 1
-        end
-        if transG ~= 0 then
-            transG = transG - 1
-        end
-    end
-end)]]
-
 RegisterNetEvent('gunshotPlace')
 AddEventHandler('gunshotPlace', function(gx, gy, gz)
     transG = 255
@@ -34,6 +23,7 @@ AddEventHandler('gunshotPlace', function(gx, gy, gz)
     SetBlipSprite(thiefBlip,  1)
     SetBlipColour(thiefBlip,  1)
     SetBlipAlpha(thiefBlip,  transG)
+    SetBlipAsShortRange(thiefBlip,  1)
     while transG ~= 0 do
         Wait(10)
         transG = transG - 1
@@ -47,17 +37,18 @@ end)
 Citizen.CreateThread( function()
     while true do
         Wait(0)
-        local male = IsPedMale(GetPlayerPed(-1))
-        if male then
-            sex = "men"
-        elseif not male then
-            sex = "women"
-        end
+        
         local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
         local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
         local street1 = GetStreetNameFromHashKey(s1)
         local street2 = GetStreetNameFromHashKey(s2)
         if IsPedTryingToEnterALockedVehicle(GetPlayerPed(-1)) or IsPedJacking(GetPlayerPed(-1)) then
+            local male = IsPedMale(GetPlayerPed(-1))
+            if male then
+                sex = "men"
+            elseif not male then
+                sex = "women"
+            end
             TriggerServerEvent('thiefInProgressPos', plyPos.x, plyPos.y, plyPos.z)
             local veh = GetVehiclePedIsTryingToEnter(GetPlayerPed(-1))
             local vehName = GetDisplayNameFromVehicleModel(GetEntityModel(veh))
@@ -76,17 +67,18 @@ end)
 Citizen.CreateThread( function()
     while true do
         Wait(0)
-        local male = IsPedMale(GetPlayerPed(-1))
-        if male then
-            sex = "men"
-        elseif not male then
-            sex = "women"
-        end
+        
         local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
         local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
         local street1 = GetStreetNameFromHashKey(s1)
         local street2 = GetStreetNameFromHashKey(s2)
         if IsPedShooting(GetPlayerPed(-1)) then
+            local male = IsPedMale(GetPlayerPed(-1))
+            if male then
+                sex = "men"
+            elseif not male then
+                sex = "women"
+            end
             TriggerServerEvent('gunshotInProgressPos', plyPos.x, plyPos.y, plyPos.z)
             if s2 == 0 then
                 TriggerServerEvent('gunshotInProgressS1', street1, sex)
