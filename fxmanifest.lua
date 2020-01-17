080f289bca45b0d3d055c8da4d9208433fab0ed0
rex2630 <rex2630@gmail.com>
2020-01-17 20:29:19 +0100

    Replace __resource.lua with fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,6 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 description 'ESX Outlaw Alert'
 
@@ -13,6 +15,7 @@ server_scripts {
 	'config.lua',
 	'server/main.lua'
 }
+
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/fr.lua',
